049a4d2ba5ecebddb618c89f36fc162d03a13400
Roberto Candelario <r-jc@live.com>
2024-08-12 18:45:09 -0400

    Initial Commit

---
+++ Navigations/NavigationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationsApp.swift
+//  Navigations
+//
+//  Created by Roberto Candelario on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NavigationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationsTests/NavigationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationsTests.swift
+//  NavigationsTests
+//
+//  Created by Roberto Candelario on 8/12/24.
+//
+
+import Testing
+@testable import Navigations
+
+struct NavigationsTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
